d961727d34723fe891f744303687e682c0bad7fd
ricardocastilloisc <ricardocastilloisc@gmail.com>
2019-01-04 16:14:49 -0500

    9.027 Middleware para configurar el idioma de nuestra aplicacion en cada peticion

--- resources/views/partials/auth/social_login.blade.php
+++ resources/views/partials/auth/social_login.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-4">
     <div class="card">
-        <div class="card-header">{{ __("Socialite")}}</div>
+        <div class="card-header">{{ __("Socialite") }}</div>
         <div class="card-body">
             <a
                 href="{{route('social_auth', ['driver' => 'github'])}}"
